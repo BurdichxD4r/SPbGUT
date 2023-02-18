c54186ba1a154e03d948422c56150b21a9a7fb84
BurdichxD4r <burdich.xd4r@gmail.com>
2023-02-18 21:18:16 +0300

    Lab_1_and_2.cpp_v_17

---
+++ first_semester/programming/laboratory_works/Lab_3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+int operatorDefinition() {
+    std::cout << "Введите символ для определения оператора (f, F, w, W, d, D)" << std::endl;
+
+    return 0;
+}
+
+int main() {
+    return 0;
+}
