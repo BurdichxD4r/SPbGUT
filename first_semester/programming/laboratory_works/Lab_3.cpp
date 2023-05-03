6938f6f8dda9e67082c69f6a4cbcf0be17f929c3
BurdichxD4r <burdich.xd4r@gmail.com>
2023-05-03 15:54:43 +0300

    Lab_3.cpp_v_17

--- first_semester/programming/laboratory_works/Lab_3.cpp
+++ first_semester/programming/laboratory_works/Lab_3.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 double funtionDoWhile() {
-    std::cout << "ok" << std::endl;
+    std::cout << std::endl;
     double y = 1, x = 0.38, N = 9;
     int i = 2;
     do {
@@ -14,7 +14,7 @@ double funtionDoWhile() {
 }
 
 double funtionWhile() {
-    std::cout << "ok" << std::endl;
+    std::cout << std::endl;
     double y = 1, x = 0.38, N = 9;
     int i = 2;
     while (i <= N){
@@ -26,7 +26,7 @@ double funtionWhile() {
 }
 
 double funtionFor() {
-    std::cout << "ok" << std::endl;
+    std::cout << std::endl;
     double y = 1, x = 0.38, N = 9;
     for (int i = 2; i <= N; i++) {
         y *= 2.8 * pow(x, 3) + 2.8 * sin(i * x);
@@ -76,6 +76,15 @@ int operatorDefinition() {
 }
 
 int main() {
+    std::cout << "Циклические вычислительные процессы. Вычисления по рекуррентным формулам." << std::endl;
+    std::cout << "Вариант 17\n" << std::endl;
+    std::cout << "Определить значение выражения с использованием 3 циклов. Для вывода значений выражения использовать оператор выбора swith()\n"
+    << "Задание: ввести с клавиатуры символ.\n"
+    << "Если этот символ - «f» или «F» - то вычислить значение y с помощью оператора FOR;\n"
+    << "если этот символ - «w» или «W» - то вычислить y операторов WHILE;\n"
+    << "если этот символ - «d» или «D» - оператором DO WHILE;\n"
+    << "если введен какой-либо другой символ — вывести сообщение «НЕВЕРНЫЙ СИМВОЛ»\n"
+    << "Вместо символов f, w или d можно вводить цифры 1,2 или 3\n" << std::endl;
     operatorDefinition();
     return 0;
 }
